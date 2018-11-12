299aef21504cf14c409fad2c2bde6213c039fe3d
Guy G <guy.gavergun@gmail.com>
2018-11-12 14:47:11 -0500

    Trigger another build

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.port || 3000;
 
 app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 });
