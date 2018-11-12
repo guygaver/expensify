5d9b037355120846c225ce17123aae71165627a6
Guy G <guy.gavergun@gmail.com>
2018-11-12 13:55:03 -0500

    Added production webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
 const path = require('path');
 const publicPath = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: publicPath,
-        filename: 'bundle.js'
-    },
-    
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ],
-            exclude: /node_modules/
-        }]
-    },
-    
-    devtool: "cheap-module-eval-source-map",
+module.exports = (env, argv) => {
 
-    devServer: {
-        contentBase: publicPath,
-        historyApiFallback: true
-    }
+    const isProduction = env === 'production';
+
+    return {
+
+        entry: './src/app.js',
+
+        output: {
+            path: publicPath,
+            filename: 'bundle.js'
+        },
+
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader',],
+            }, {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                exclude: /node_modules/
+            }]
+        },
+
+        devtool: isProduction ? "source-map" : "inline-source-map",
+
+        devServer: {
+            contentBase: publicPath,
+            historyApiFallback: true
+        }
+    };
 };
